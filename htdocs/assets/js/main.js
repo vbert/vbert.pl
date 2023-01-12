39308020a58501ce1f8b66d704292bc57976502f
Wojciech Sobczak <wsobczak@gmail.com>
2023-01-13 00:39:27 +0100

    Basic file structure

---
+++ htdocs/assets/js/main.js
@@ -0,0 +1,11 @@
+/*
+ * Project: vbert.pl
+ * File: main.js
+ * File Created: 2023-01-12, 23:57:23
+ * Author: Wojciech Sobczak (wsobczak@gmail.com)
+ * -----
+ * Last Modified: 2023-01-13, 0:25:30
+ * Modified By: Wojciech Sobczak (wsobczak@gmail.com)
+ * -----
+ * Copyright © 2021 - 2023 by vbert
+ */
